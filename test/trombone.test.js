c95554eb20e83ec196fb9b741718b6234e378659
matt-barton <matt@mattbarton.org>
2015-04-01 09:57:35 +0100

    Initial scaffolding

---
+++ test/trombone.test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var trombone = require('../trombone.js');
+
+describe('trombone', function() {
+	it ('works', function(done) {
+		done();
+	});
+});
---
+++ trombone.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var request = require('request');
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+module.exports = function() {
+	return {
+
+	};
+}

900e02aa4cead3bf3789c8f6d9b3af5445573c24
matt-barton <matt@mattbarton.org>
2015-04-01 16:57:18 +0100

    Initial test/dev of trombone.observe

--- test/trombone.test.js
+++ test/trombone.test.js
@@ -1,9 +1,107 @@
 "use strict";
 
-var trombone = require('../trombone.js');
+var should = require('should');
+var tromboneModule = require('../trombone.js');
 
 describe('trombone', function() {
-	it ('works', function(done) {
-		done();
+
+	describe('observe', function() {
+
+		var mockFs = {
+			readFile: function(filename, encoding, callback) {
+				callback();
+			}
+		};
+
+		var mockRequest = function(url, callback) {
+			callback();
+		};
+
+		it ('When trombone is observing ' + 
+			'Then trombone is asked to observe.', function(done){
+			var trombone = new tromboneModule(mockFs, mockRequest);
+			trombone.observe();
+			done();
+		});
+
+		it ('Given observing is complete ' + 
+			'When trombone is observing ' + 
+			'Then the provided callback is called.', function(done){
+			var trombone = new tromboneModule(mockFs, mockRequest);
+			trombone.observe(function() {
+				done();
+			});
+		});
+
+		it ('When trombone is observing ' + 
+			'Then the current scum filter list is read from the file system', function(done) {
+			var mockFs = {
+				readFile: function(filename, encoding, callback) {
+					filename.should.equal('scumfilter.js');
+					encoding.should.equal('utf8');
+					callback();
+				}
+			};
+			var trombone = new tromboneModule(mockFs, mockRequest);
+			trombone.observe(function() {
+				done();
+			});
+		});
+
+		it ('Given an error occurs ' + 
+			'When the current scum filter is read ' +
+			'Then the error is passed to the callback', function(done){
+			var error = new Error();
+			var mockFs = {
+				readFile: function(filename, encoding, callback) {
+					callback(error);
+				}
+			};
+			var trombone = new tromboneModule(mockFs, mockRequest);
+			trombone.observe(function(e) {
+				e.should.equal(error);
+				done();
+			});
+		});
+
+		it ('Given an error occurs ' + 
+			'When the current scum filter is read ' +
+			'Then the wroth index page is not requested from the web', function(done){
+			var error = new Error();
+			var mockFs = {
+				readFile: function(filename, encoding, callback) {
+					callback(error);
+				}
+			};
+			var mockRequest = function(url, callback) {
+				(true).should.equal(false);
+				callback();
+			};
+			var trombone = new tromboneModule(mockFs, mockRequest);
+			trombone.observe(function() {
+				done();
+			});
+		});
+
+		it ('Given an error occurs ')
+		it ('When trombone is observing ' + 
+			'Then the wroth index page is requested from the web', function(done) {
+			var mockRequest = function(url, callback) {
+				url.should.equal('http://www.wrathofthebarclay.co.uk/interactive/board/board.php');
+				callback();
+			};
+			var trombone = new tromboneModule(mockFs, mockRequest);
+			trombone.observe(function() {
+				done();
+			});
+		});
+
 	});
+
+	describe('parp', function() {
+		it ('no tests yet', function(done) {
+			done();
+		});
+	});
+
 });
--- trombone.js
+++ trombone.js
@@ -1,11 +1,34 @@
 "use strict";
 
-var request = require('request');
-var fs = require('fs');
-var cheerio = require('cheerio');
+var waterfall = require('async-waterfall');
 
-module.exports = function() {
+module.exports = function(fs, rq) {
+
+	function observe(callback) {
+
+		waterfall([
+
+			function(next) {
+				fs.readFile('scumfilter.js', 'utf8', function(e) {
+					if (e) {
+						if (typeof callback == 'function') return callback(e);
+					}
+					next();
+				});
+			},
+
+			function(next) {
+				rq('http://www.wrathofthebarclay.co.uk/interactive/board/board.php', function() {
+					next();
+				});
+			}
+		], function() {
+			if (typeof callback == 'function') callback();
+		});
+
+	}
+
 	return {
-
+		observe: observe
 	};
 }

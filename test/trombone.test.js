f804a004970e72633d381d61facc71aff45b9e9a
matt-barton <matt@mattbarton.org>
2015-04-02 16:54:26 +0100

    Progress ...

--- test/trombone.test.js
+++ test/trombone.test.js
@@ -7,7 +7,7 @@ describe('trombone', function() {
 
 	var mockFs = {
 		readFile: function(filename, encoding, callback) {
-			callback()
+			callback(null, '[]')
 		}
 	}
 
@@ -39,7 +39,7 @@ describe('trombone', function() {
 				readFile: function(filename, encoding, callback) {
 					filename.should.equal('scumfilter.js')
 					encoding.should.equal('utf8')
-					callback()
+					callback(null, '[]')
 				}
 			}
 			var trombone = new tromboneModule(mockFs, mockRequest)
@@ -91,12 +91,19 @@ describe('trombone', function() {
 			error.code = 'ENOENT'
 			var mockFs = {
 				readFile: function(filename, encoding, callback) {
-					callback(error)
+					callback(error, '[]')
+				},
+
+				writeFile: function(filename, data, callback) {
+					callback(null)
 				}
+			}
+			var mockRequest = function(url, callback) {
+				callback(null, 'var scum = \'["test"]\'.evalJSON')
 			}
 			var trombone = new tromboneModule(mockFs, mockRequest)
 			trombone.observe(function(e) {
-				(e === null).should.equal.true
+				(e === null).should.equal(true)
 				done()
 			})
 		})
@@ -109,15 +116,72 @@ describe('trombone', function() {
 			error.code = 'ENOENT'
 			var mockFs = {
 				readFile: function(filename, encoding, callback) {
-					callback(error)
+					callback(error, '[]')
+				},
+
+				writeFile: function(filename, data, callback) {
+					filename.should.equal('scumfilter.js')
+					data.should.equal('[]');
+					callback();
 				}
 			}
 			var trombone = new tromboneModule(mockFs, mockRequest)
 			trombone.observe(function(e) {
-				//done()
+				done()
 			})
 		})
 
+		it ('Given an error occurs ' +
+			'And the error is ENOENT ' +
+			'When the current scum filter is read ' +
+			'And an error occurs when a blank scum filter is written ' +
+			'Then the second error is passed to the callback', function(done){
+			var errorOne = new Error
+			errorOne.code = 'ENOENT'
+			var errorTwo = new Error
+			var mockFs = {
+				readFile: function(filename, encoding, callback) {
+					callback(errorOne, '[]')
+				},
+
+				writeFile: function(filename, data, callback) {
+					callback(errorTwo)
+				}
+			}
+			var trombone = new tromboneModule(mockFs, mockRequest)
+			trombone.observe(function(e) {
+				e.should.equal(errorTwo)
+				done()
+			})
+		})
+
+		it ('Given an error occurs ' +
+			'And the error is ENOENT ' +
+			'When the current scum filter is read ' +
+			'And an error occurs when a blank scum filter is written ' +
+			'Then the wroth index page is not requested from the web ', function(done){
+			var errorOne = new Error
+			errorOne.code = 'ENOENT'
+			var errorTwo = new Error
+			var mockFs = {
+				readFile: function(filename, encoding, callback) {
+					callback(errorOne, '[]')
+				},
+
+				writeFile: function(filename, data, callback) {
+					callback(errorTwo)
+				}
+			}
+			var mockRequest = function(url, callback) {
+				(true).should.equal(false)
+				callback()
+			}
+			var trombone = new tromboneModule(mockFs, mockRequest)
+			trombone.observe(function() {
+				done()
+			})
+		})
+		
 		it ('When trombone is observing ' + 
 			'Then the wroth index page is requested from the web', function(done) {
 			var mockRequest = function(url, callback) {
@@ -129,6 +193,128 @@ describe('trombone', function() {
 				done()
 			})
 		})
+		
+		it ('Given an error occurs ' + 
+			'When the wroth index page is requested from the web ' + 
+			'Then the error is passed back to the caller', function(done) {
+			var error = new Error
+			var mockRequest = function(url, callback) {
+				callback(error)
+			}
+			var trombone = new tromboneModule(mockFs, mockRequest)
+			trombone.observe(function(e) {
+				e.should.equal(error)
+				done()
+			})
+		})
+
+		// web has no entries in scum filter
+		it ('Given no scum filter entries are found ' + 
+			'When the wroth index page is requested from the web ' +
+			'Then an error is returned to the caller', function(done){
+			var mockRequest = function(url, callback) {
+				callback(null, '')
+			}
+			var trombone = new tromboneModule(mockFs, mockRequest)
+			trombone.observe(function(e) {
+				e.should.be.type('object')
+				e.message.should.equal('No scum filter entries could be found.')
+				done()
+			})
+		});
+
+		// web has one entry in scum filter, fs has one matching entry
+		// web has no entries in scum filter
+		it ('Given one scum filter entry is found on the wroth ' + 
+			'And one matching entry is found on the file system ' +
+			'Then the trombone is not asked to parp ' +
+			'And the scum filter on the filesystem is not written to', function(done){
+			var mockFs = {
+				readFile: function(filename, encoding, callback) {
+					var scumFilter = JSON.stringify(["ENTRY_ONE"])
+					callback(null, scumFilter)
+				},
+				writeFile: function(filename, data, callback) {
+					(false).should.equal(true)
+					callback()
+				}
+			}
+			var mockRequest = function(url, callback) {
+				callback(null, 'var scum = \'["ENTRY_ONE"]\'.evalJSON')
+			}
+			var trombone = new tromboneModule(mockFs, mockRequest)
+			trombone.parp = function() {
+				(false).should.equal(true)
+			}
+			trombone.observe(function() {
+				done()
+			})
+		});
+
+		it ('Given more than one scum filter entry is found on the wroth' + 
+			'And matching entries are found on the file system ' +
+			'Then the trombone is not asked to parp ' +
+			'And the scum filter on the filesystem is not written to', function(done){
+			var mockFs = {
+				readFile: function(filename, encoding, callback) {
+					var scumFilter = JSON.stringify(["ENTRY_ONE", "ENTRY_TWO", "ENTRY_THREE"])
+					callback(null, scumFilter)
+				},
+				writeFile: function(filename, data, callback) {
+					(false).should.equal(true)
+					callback()
+				}
+			}
+			var mockRequest = function(url, callback) {
+				callback(null, 'var scum = \'["ENTRY_ONE","ENTRY_TWO","ENTRY_THREE"]\'.evalJSON')
+			}
+			var trombone = new tromboneModule(mockFs, mockRequest)
+			trombone.parp = function() {
+				(false).should.equal(true)
+			}
+			trombone.observe(function() {
+				done()
+			})
+		});
+
+		it ('Given more than one scum filter entry is found on the wroth ' + 
+			'And all but one matching entries are found on the file system ' +
+			'Then the trombone is asked to parp ' +
+			'And the scum filter on the filesystem is updated', function(done){
+			var writeFileCalled = false;
+			var parpCalled = false;
+			var webScumfilter = JSON.parse('["ENTRY_ONE","ENTRY_TWO","ENTRY_THREE"]')
+			var mockFs = {
+				readFile: function(filename, encoding, callback) {
+					var scumFilter = JSON.stringify(["ENTRY_ONE", "ENTRY_TWO"])
+					callback(null, scumFilter)
+				},
+				writeFile: function(filename, data, callback) {
+					data.should.equal(JSON.strigify(webScumfilter))
+					writeFile = true;
+					callback()
+				}
+			}
+			var mockRequest = function(url, callback) {
+				callback(null, 'var scum = \'["ENTRY_ONE","ENTRY_TWO","ENTRY_THREE"]\'.evalJSON')
+			}
+			var trombone = new tromboneModule(mockFs, mockRequest)
+			trombone.parp = function(message) {
+				message.should.equal("ENTRY_THREE added to Scum Filter.")
+				parpCalled = true;
+			}
+			trombone.observe(function() {
+				parpCalled.should.equal(true);
+				writeFileCalled.should.equal(true);
+				done()
+			})
+		});
+
+
+		// web has > one entry in scum filter, fs has all but one matching entries
+		// web has > one entry in scum filter, fs has > zero entries, > one of which don't match
+		// web has > one entry in scum filter, fs has all matching entries, plus one extra
+		// web has > one entry in scum filter, fs has all matching entries, plus > one extra
 
 	})
 
--- trombone.js
+++ trombone.js
@@ -9,25 +9,95 @@ module.exports = function(fs, rq) {
 		waterfall([
 
 			function(next) {
-				fs.readFile('scumfilter.js', 'utf8', function(e) {
+				fs.readFile('scumfilter.js', 'utf8', function(e, data) {
 					if (e) {
 						if (e.code == 'ENOENT') {
-
+							var file = JSON.stringify([])
+							return fs.writeFile('scumfilter.js', file, function(e) {
+								if (e) if (typeof callback == 'function') return callback(e)
+								return next(null, JSON.parse(file))
+							})
 						}
 						else {
 							if (typeof callback == 'function') return callback(e)
 						}
 					}
-					next()
+					next(null, JSON.parse(data))
 				})
 			},
 
-			function(next) {
-				rq('http://www.wrathofthebarclay.co.uk/interactive/board/board.php', function() {
-					next()
+			function(fsScumFilter, next) {
+				rq('http://www.wrathofthebarclay.co.uk/interactive/board/board.php', function(e, html) {
+					if (e) if (typeof callback == 'function') return callback(e)
+					function fail() {
+						callback(new Error('No scum filter entries could be found.'))
+					}
+					var regex = /^.*var scum = '(\[.+\])'\.evalJSON.*$/
+					var scum = []
+					try {
+						var matches = regex.exec(html);
+						if (matches == null) return fail()
+						scum = JSON.parse(matches[1]);
+						if (scum.length == 0) {
+							if (typeof callback == 'function') fail()
+							return
+						}
+					}
+					catch (e) {
+						if (typeof callback == 'function') return fail()
+					}
+
+					next(null, fsScumFilter, scum)
+				})
+			},
+
+			function(fsScumFilter, webScumFilter, next) {
+				waterfall(webScumFilter.map(
+					function(webFilterEntry){ 
+						return function (lastResult, webCallback) {
+							if (typeof lastResult == 'function') webCallback = lastResult
+							if (typeof lastResult != 'array') lastResult = []
+							waterfall(fsScumFilter.map(
+								function(fsFilterEntry) {
+									return function (last, fsCallback) {
+										if (typeof last == 'function') fsCallback = last
+										if (last === true) return fsCallback(null, true)
+										if (webFilterEntry == fsFilterEntry) return fsCallback(null, true)
+										fsCallback(null, false);
+									}
+								}),
+								function(e, found) {
+									if (!found) {
+										lastResult.push(webFilterEntry);
+									}
+									webCallback(null, lastResult)
+								} 
+							)
+						}
+					}),
+					function(e, newEntries) {
+						next(null, newEntries)
+					}
+				)
+			},
+
+			function(newScumFilterEntries, next) {
+				waterfall(newScumFilterEntries.map(
+					function(entry) {
+						return function(last, nextCallback) {
+							if (typeof last == 'function') nextCallback = last;
+							nextCallback()
+						}
+					}
+					
+				),
+				function (e) {
+					next(null)
 				})
 			}
-		], function() {
+
+		], function(e) {
+			if (e) if (typeof callback == 'function') return callback(e)
 			if (typeof callback == 'function') callback(null)
 		})
 

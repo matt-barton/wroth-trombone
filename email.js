5f5d4982ad8abc75545da4e12bc7ff7b4aa79b48
matt-barton <matt@mattbarton.org>
2015-04-07 16:57:01 +0100

    Development and testing of email module

--- email.js
+++ email.js
@@ -1,15 +1,44 @@
 "use strict"
 
-module.exports = function() {
+module.exports = function(nodemailer) {
 
-	function error () {
+	var transport = null
 
+	function createTransport() {
+		if (transport != null) return
+		transport = nodemailer.createTransport({
+			service: process.env.WROTH_TROMBONE_EMAIL_SERVICE,
+			auth: {
+				user: process.env.WROTH_TROMBONE_EMAIL_USERNAME,
+				pass: process.env.WROTH_TROMBONE_EMAIL_PASSWORD
+			}
+		})
 	}
 
-	function resumption() {
-
+	function error (callback) {
+		createTransport()
+		transport.sendMail({
+			from:    process.env.WROTH_TROMBONE_EMAIL_USERNAME,
+			to:      process.env.WROTH_TROMBONE_EMAIL_RECIPIENT,
+			subject: 'Wroth Trombone: Error',
+			text:    'This is the Wroth Trombone.  I encountered an error reading the Scum Filter from Wroth.'
+		}, function() {
+			if (typeof callback == 'function') return callback()
+		})
 	}
-	
+
+	function resumption(callback) {
+		createTransport()
+		transport.sendMail({
+			from:    process.env.WROTH_TROMBONE_EMAIL_USERNAME,
+			to:      process.env.WROTH_TROMBONE_EMAIL_RECIPIENT,
+			subject: 'Wroth Trombone: Service Resumed',
+			text:    'This is the Wroth Trombone.  Previous errors have cleared, and service has resumed.'
+		}, function() {
+			if (typeof callback == 'function') return callback()
+		})
+	}
+
 	return {
 		error: error,
 		resumption: resumption
--- test/email.test.js
+++ test/email.test.js
@@ -1,11 +1,233 @@
 "use strict"
 
 var should = require('should')
-var parp = require('../email')
+var emailModule = require('../email')
 
 describe('trombone', function() {
 
-	describe('parp', function() {
+	describe('email', function() {
 
+		// Given that no transport exists
+		// When an error email is requested
+		// Then a new transport is created
+		it ('Given that no transport exists ' +
+			'When an error email is requested ' +
+			'Then a new transport is created', function(done) {
+			var transportCreated = false
+			var mockMailer = {
+				createTransport: function() {
+					transportCreated = true
+					return {
+						sendMail: function(options, callback) {
+							if (typeof callback == 'function') return callback()
+						}
+					}
+				}
+			}
+			var email = new emailModule(mockMailer)
+			email.error(function() {
+				transportCreated.should.be.true
+				done()
+			})
+		})
+
+		// Given that a transport has already been created
+		// When an error email is requested
+		// Then a new transport is created
+		it('Given that a transport has already been created ' +
+			'When an error email is requested ' +
+			'Then a new transport is created', function(done){
+			var transportCreatedCount = 0
+			var mockMailer = {
+				createTransport: function() {
+					transportCreatedCount++
+					return {
+						sendMail: function(options, callback) {
+							if (typeof callback == 'function') return callback()
+						}
+					}
+				}
+			}
+			var email = new emailModule(mockMailer)
+			email.error(function() {
+				email.error(function() {
+					transportCreatedCount.should.equal(1)
+					done()
+				})
+			})
+		})
+
+		// Given that no transport exists
+		// When a resumption email is requested
+		// Then a new transport is created
+		it ('Given that no transport exists ' +
+			'When a resumption email is requested ' +
+			'Then a new transport is created', function(done) {
+			var transportCreated = false
+			var mockMailer = {
+				createTransport: function() {
+					transportCreated = true
+					return {
+						sendMail: function(options, callback) {
+							if (typeof callback == 'function') return callback()
+						}
+					}
+				}
+			}
+			var email = new emailModule(mockMailer)
+			email.resumption(function() {
+				transportCreated.should.be.true
+				done()
+			})
+		})
+
+		// Given that a transport has already been created
+		// When a resumption email is requested
+		// Then a new transport is created
+		it('Given that a transport has already been created ' +
+			'When a resumption email is requested ' +
+			'Then a new transport is created', function(done){
+			var transportCreatedCount = 0
+			var mockMailer = {
+				createTransport: function() {
+					transportCreatedCount++
+					return {
+						sendMail: function(options, callback) {
+							if (typeof callback == 'function') return callback()
+						}
+					}
+				}
+			}
+			var email = new emailModule(mockMailer)
+			email.resumption(function() {
+				email.resumption(function() {
+					transportCreatedCount.should.equal(1)
+					done()
+				})
+			})
+		})
+
+		// Given that no transport exists
+		// When an error email is requested
+		// Then a new transport is created with details from environment variables
+		it ('Given that no transport exists ' +
+			'When an error email is requested ' +
+			'Then a new transport is created with details from environment variables', function(done) {
+			var transportCreated = false
+			process.env.WROTH_TROMBONE_EMAIL_SERVICE = 'EMAIL_SERVICE'
+			process.env.WROTH_TROMBONE_EMAIL_USERNAME = 'EMAIL_USERNAME'
+			process.env.WROTH_TROMBONE_EMAIL_PASSWORD = 'EMAIL_PASSWORD'
+
+			var mockMailer = {
+				createTransport: function(options) {
+					transportCreated = true
+					options.should.be.type('object')
+					options.service.should.equal('EMAIL_SERVICE')
+					options.auth.should.be.type('object')
+					options.auth.user.should.equal('EMAIL_USERNAME')
+					options.auth.pass.should.equal('EMAIL_PASSWORD')
+					return {
+						sendMail: function(options, callback) {
+							if (typeof callback == 'function') return callback()
+						}
+					}
+				}
+			}
+			var email = new emailModule(mockMailer)
+			email.error(function() {
+				transportCreated.should.be.true
+				done()
+			})
+		})
+
+		// Given that no transport exists
+		// When a resumption email is requested
+		// Then a new transport is created with details from environment variables
+		it ('Given that no transport exists ' +
+			'When a resumption email is requested ' +
+			'Then a new transport is created with details from environment variables', function(done) {
+			var transportCreated = false
+			process.env.WROTH_TROMBONE_EMAIL_SERVICE = 'EMAIL_SERVICE'
+			process.env.WROTH_TROMBONE_EMAIL_USERNAME = 'EMAIL_USERNAME'
+			process.env.WROTH_TROMBONE_EMAIL_PASSWORD = 'EMAIL_PASSWORD'
+			var mockMailer = {
+				createTransport: function(options) {
+					transportCreated = true
+					options.should.be.type('object')
+					options.service.should.equal('EMAIL_SERVICE')
+					options.auth.should.be.type('object')
+					options.auth.user.should.equal('EMAIL_USERNAME')
+					options.auth.pass.should.equal('EMAIL_PASSWORD')
+					return {
+						sendMail: function(options, callback) {
+							if (typeof callback == 'function') return callback()
+						}
+					}
+				}
+			}
+			var email = new emailModule(mockMailer)
+			email.resumption(function() {
+				transportCreated.should.be.true
+				done()
+			})
+		})
+
+		// When an error email is requested
+		// Then an error email is sent
+		it('When an error email is requested ' + 
+			'Then an error email is sent', function(done){
+			process.env.WROTH_TROMBONE_EMAIL_USERNAME = 'EMAIL_USERNAME'
+			process.env.WROTH_TROMBONE_EMAIL_RECIPIENT = 'EMAIL_RECIPIENT'
+			var emailSent = false
+			var mockMailer = {
+				createTransport: function () {
+					return {
+						sendMail: function (options, callback) {
+							emailSent = true
+							options.should.be.type('object')
+							options.from.should.equal('EMAIL_USERNAME')
+							options.to.should.equal('EMAIL_RECIPIENT')
+							options.subject.should.equal('Wroth Trombone: Error')
+							options.text.should.equal('This is the Wroth Trombone.  I encountered an error reading the Scum Filter from Wroth.')
+							callback()
+						}
+					}
+				}
+			}
+			var email = new emailModule(mockMailer)
+			email.error(function() {
+				emailSent.should.be.true
+				done()
+			})
+		})
+
+		// When a resumption email is requested
+		// Then a resumption email is sent
+		it('When an error email is requested ' + 
+			'Then an error email is sent', function(done){
+			process.env.WROTH_TROMBONE_EMAIL_USERNAME = 'EMAIL_USERNAME'
+			process.env.WROTH_TROMBONE_EMAIL_RECIPIENT = 'EMAIL_RECIPIENT'
+			var emailSent = false
+			var mockMailer = {
+				createTransport: function () {
+					return {
+						sendMail: function (options, callback) {
+							emailSent = true
+							options.should.be.type('object')
+							options.from.should.equal('EMAIL_USERNAME')
+							options.to.should.equal('EMAIL_RECIPIENT')
+							options.subject.should.equal('Wroth Trombone: Service Resumed')
+							options.text.should.equal('This is the Wroth Trombone.  Previous errors have cleared, and service has resumed.')
+							callback()
+						}
+					}
+				}
+			}
+			var email = new emailModule(mockMailer)
+			email.resumption(function() {
+				emailSent.should.be.true
+				done()
+			})
+		})
 	})
 })

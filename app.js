cbf3fcc1ddd3070a41d336e4d42ddc90d9a49349
Matt Barton <matt@mattbarton.org>
2015-04-09 22:13:29 +0100

    temp test for heroku

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 "use strict"
 
-
 var fs = require('fs')
 var rq = require('request')
-var parp = require('./parp')
+
+var twit = require('twit')
+var parp = require('./parp')(twit)
+
+parp('test #2')
+
+/*
 var nm = require('nodemailer')
 var smtpTransport = require('nodemailer-smtp-transport');
-
 var emailModule = require('./email')
 var email = new emailModule(nm, smtpTransport)
 
@@ -18,13 +22,12 @@ var seconds = process.env.WROTH_TROMBONE_OBSERVE_FREQUENCY
 function trombone () {
 	setTimeout(function() {
 		observe.go(function(e){
-			console.error(e)
 			trombone()
 		})		
 	}, seconds * 1000)
 }
 trombone()
-
+*/
 
 
 
